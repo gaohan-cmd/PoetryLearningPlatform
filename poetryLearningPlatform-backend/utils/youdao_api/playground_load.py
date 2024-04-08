85d8b42e4f5b070fde4114478e8d586da367ec79
sjc <810233122@qq.com>
2024-04-08 22:52:08 +0800

    一次加载模型

--- poetryLearningPlatform-backend/utils/youdao_api/playground_load.py
+++ poetryLearningPlatform-backend/utils/youdao_api/playground_load.py
@@ -8,7 +8,7 @@ import config
 from PIL import Image
 from io import BytesIO
 from transformers import TextStreamer
-from load_text2img_model import pipe
+from app import pipe
 from utils.youdao_api.AuthV3Util import addAuthParams
 import config
 # from AuthV3Util import addAuthParams
